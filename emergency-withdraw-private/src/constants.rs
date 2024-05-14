96c425dbc1f7f67497390dcf8a9560a1c1b7f786
0xHumban <75947702+0xHumban@users.noreply.github.com>
2024-05-14 15:35:01 +0200

    add refresh wallet balance user event

--- emergency-withdraw-private/src/constants.rs
+++ emergency-withdraw-private/src/constants.rs
@@ -9,5 +9,5 @@ pub const WALLETS_NUMBER_VAR_NAME: &str = "WALLETS_NUMBER";
 // ui constants
 pub const APP_TITLE: &str = " EMERGENCY WITHDRAW ";
 pub const INFO_TEXT: &str =
-    " (q) quit | (↓  / ↑ ) move | (Enter) select | (a) select all | (t) transfer ";
+    " (q) quit | (↓  / ↑ ) move | (Enter) select | (a) all | (t) transfer | (r) refresh ";
 pub const INFO_TEXT_CONFIRM_TRANSFER: &str = " (ESC) quit | ( → ) next | (Enter) select ";
